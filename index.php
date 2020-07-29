863fabcc0d044e744948789854ab3c6484ed2e8e
Krishna Teja <packetcode@gmail.com>
2020-07-29 14:19:17 +0530

    Some changes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if(isset($_REQUEST['name'])){
     list($type, $data) = explode(';', $data);
     list(, $data)      = explode(',', $data);
     $data = base64_decode($data);
-    $path = '/images/'.$name.'.png';
+    $path = 'images/'.$name.'.png';
     file_put_contents($path, $data);
     $cmd = 'python3 yolo.py --image '.$path.' --yolo yolo-coco';
     $count = shell_exec($cmd);
